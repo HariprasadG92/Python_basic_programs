d67760b3ee0bedc7118496d32c0fed50a6d8415b
Hariprasad G <101337307+HariprasadG92@users.noreply.github.com>
2023-02-04 20:15:23 +0530

    Add files via upload

---
+++ Life_in_Weeks_.py
@@ -0,0 +1,7 @@
+print("program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.")
+age = input("What is your current age? ")
+age1 = 90-int(age)
+days = int(age1)*365
+weeks = int(age1)*52
+months = int(age1)*12
+print("You have "+str(days)+" days, "+str(weeks)+" weeks, and "+str(months)+" months left.")
