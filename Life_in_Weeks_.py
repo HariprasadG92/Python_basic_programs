b14157f1dd769dc0f87f3beb2b70293334ed6f08
Hariprasad G <101337307+HariprasadG92@users.noreply.github.com>
2023-02-04 20:21:42 +0530

    Add files via upload

--- Life_in_Weeks_.py
+++ Life_in_Weeks_.py
@@ -4,4 +4,4 @@ age1 = 90-int(age)
 days = int(age1)*365
 weeks = int(age1)*52
 months = int(age1)*12
-print("You have "+str(days)+" days, "+str(weeks)+" weeks, and "+str(months)+" months left.")
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
