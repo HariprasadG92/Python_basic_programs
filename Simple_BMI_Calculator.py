bc9c9a59db71183ece06b671b8c995e4c591affb
Hariprasad G <101337307+HariprasadG92@users.noreply.github.com>
2023-02-06 22:37:21 +0530

    Update Simple_BMI_Calculator.py

--- Simple_BMI_Calculator.py
+++ Simple_BMI_Calculator.py
@@ -3,5 +3,5 @@ print("Enter Height and Weight for BMI calculation")
 height = int(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
 bmi = weight/height**2
-print(bmi_as_int)
+print(int(bmi))
 print("BMI values less than 18.5 kg/m² are considered underweight.\nBMI values from 18.5 kg/m² to 24.9 kg/m² are healthy.\nOverweight is defined as a body mass index of 25.0 to less than 30.0 kg/m²")
